139a9829536371d1426c4e49ac4af03646d4d697
David Riggins <driggins@esri.com>
2022-11-29 12:42:05 -0800

    Start of Dropdown.vue component.

---
+++ vue-multiselect/src/components/Dropdown.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="dropdown-list">
+    <a href="#">{{ title }} &#11206; </a>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const title = ref("Hello");
+</script>
--- vue-multiselect/src/views/HomeView.vue
+++ vue-multiselect/src/views/HomeView.vue
@@ -1,5 +1,30 @@
 <template>
   <div>
-    <h1>Home</h1>
+    <DropdownVue></DropdownVue>
+    <!-- <h1>Home</h1>
+    <ul>
+      <li
+        class="list-item"
+        v-for="(option, index) of filteredOptions"
+        :key="index"
+      >
+        <span><input type="checkbox" /> option</span>
+      </li>
+    </ul> -->
   </div>
 </template>
+
+<script setup>
+//import { ref } from "vue";
+import DropdownVue from "../components/Dropdown.vue";
+
+//const filteredOptions = ref(["Hello1", "Hello2", "Hello3"]);
+</script>
+
+<style scoped>
+@import "@/assets/styles/project.css";
+
+.list-item {
+  list-style-type: none;
+}
+</style>
