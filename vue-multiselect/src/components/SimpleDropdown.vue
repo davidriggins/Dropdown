c67a53567b6f9fb6f56c03fce32d6036f1e6485a
David Riggins <driggins@esri.com>
2022-12-20 11:11:19 -0800

    More work on SimpleDropdown component.

--- vue-multiselect/src/components/SimpleDropdown.vue
+++ vue-multiselect/src/components/SimpleDropdown.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="qam-simple-dropdown">
-    <Button @click="toggleDropdown" class="qam-simple-dropdown-button"> {{ buttonText }} </Button>
+    <Button @click="toggleDropdown" class="qam-simple-dropdown-button">
+      {{ buttonLabel + "  ⮟" }}
+    </Button>
     <ul v-if="show" class="qam-simple-dropdown-content">
-      <li v-for="(item, index) in list" :key="index">{{ item }}</li>
+      <li v-for="(item, index) in items" :key="index" @click="itemWasSelected(item)">{{ item }}</li>
     </ul>
   </div>
 </template>
@@ -11,21 +13,57 @@
 import { ref, onMounted } from "vue";
 import Button from "@/components/Button.vue";
 
+const props = defineProps({
+  // The list of items in the dropdown.
+  items: {
+    type: Array,
+  },
+
+  // The (original) button text you want.
+  buttonText: {
+    type: String,
+    default: "Select",
+  },
+
+  // Keep the selected value as the button text.
+  keepSelectedAsText: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+// const slots = useSlots();
+// const slotText = computed(() => {
+//   return slots.default()[0].children;
+// });
+
 const show = ref(false);
-const list = ref(["5", "10", "25", "100", "200"]);
-const buttonText = ref("");
+const buttonLabel = ref("");
+const itemSelected = ref("");
+// const list = ref(["5", "10", "25", "100", "200"]);
+// const buttonText = ref("Select");
 
 onMounted(() => {
-  buttonText.value = list.value[0] + "  ⮟";
+  buttonLabel.value = props.buttonText;
 });
 
 const toggleDropdown = () => {
   show.value = !show.value;
 };
+
+const itemWasSelected = (item) => {
+  show.value = !show.value;
+  itemSelected.value = item;
+
+  if (props.keepSelectedAsText) {
+    buttonLabel.value = item;
+  }
+  console.log("Item selected: ", item);
+};
 </script>
 
 <style scoped>
-/* *,
+*,
 *::before,
 *::after {
   box-sizing: border-box;
@@ -33,14 +71,15 @@ const toggleDropdown = () => {
   padding: 0;
   position: relative;
   font-weight: normal;
-} */
+}
 
 Button {
   cursor: pointer;
   min-width: 50px;
 }
+
 div {
-  margin: 0.3rem 0.3rem 0.3rem 2rem;
+  margin: 0;
   position: relative;
   display: inline-block;
 }
@@ -51,17 +90,31 @@ ul {
   margin: 0;
   padding: 0;
   /* width: fit-content; */
-  overflow: auto;
+  width: 100%;
+
+  /* Scrolling in dropdown */
+  max-height: 30rem; /* Maximum height of viewable content */
+  overflow: hidden; /* Hides the overflow content */
+  overflow-y: auto; /* Sets y scroll bars */
   z-index: 1;
 }
 
 li {
   list-style-type: none;
   margin: 0;
-  padding: 0;
+  padding: 0 0.3rem;
   width: 100%;
   display: block;
   z-index: 1;
+  border: 2px solid #fff;
+}
+
+li:hover {
+  background-color: #ccc;
+}
+
+li:active {
+  border: 2px solid #00f;
 }
 
 /* .qam-simple-dropdown {
--- vue-multiselect/src/views/AboutView.vue
+++ vue-multiselect/src/views/AboutView.vue
@@ -1,15 +1,20 @@
 <template>
-  <div class="text-center mt-5 mb-5">
-    <h1 class="mt-5 fw-bolder text-success">Component Tests</h1>
-    <div class="mt-5">
-      <Table2></Table2>
-    </div>
+  <!-- <div class="text-center mt-5 mb-5">
+    <h1 class="mt-5 fw-bolder text-success">Component Tests</h1> -->
+  <div class="mt-5">
+    <!-- <Table2></Table2> -->
+    <SimpleDropdown :items="list" :keepSelectedAsText="true"></SimpleDropdown>
   </div>
+  <!-- </div> -->
 </template>
 
 <script setup>
+import { ref } from "vue";
 import Table2 from "@/components/Table2.vue";
+import SimpleDropdown from "@/components/SimpleDropdown.vue";
 import { useStoreTable } from "@/stores/storeTable";
+
+const list = ref(["5", "10", "25", "100", "200"]);
 </script>
 
 <style scoped>
