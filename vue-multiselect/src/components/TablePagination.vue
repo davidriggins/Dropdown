523c72e5bdc0ca6ff10570c04aeffea3f4cf0237
David Riggins <driggins@esri.com>
2022-12-28 14:55:46 -0800

    Got the button numbers to work properly. Commented that section too.

--- vue-multiselect/src/components/TablePagination.vue
+++ vue-multiselect/src/components/TablePagination.vue
@@ -4,8 +4,14 @@
       <li>
         <Button class="qam-previous" :disabled="prevDisabled">Previous</Button>
       </li>
-      <li v-for="index in numButtons" :key="index">
-        <Button class="qam-page-button" name="qam-page-button" @click="pageButtonClicked(buttonNumbers[index - 1], $event)">{{ buttonNumbers[index - 1] }}</Button>
+      <li v-for="index in numberOfButtons" :key="index">
+        <Button
+          class="qam-page-button"
+          name="qam-page-button"
+          :class="centerNumber == buttonNumbers[index - 1] ? 'activeButton' : 'inactiveButton'"
+          @click="pageButtonClicked(buttonNumbers[index - 1], $event)"
+          >{{ buttonNumbers[index - 1] }}</Button
+        >
       </li>
       <li>
         <Button class="qam-next" :disabled="nextDisabled">Next</Button>
@@ -21,16 +27,15 @@ import Button from "@/components/Button.vue";
 import SimpleDropdown from "@/components/SimpleDropdown.vue";
 
 // const radius = ref(2);
-const numButtons = ref(0);
+const numberOfButtons = ref(0);
 const buttonNumbers = ref([]);
 const currentPage = ref(1);
 const prevDisabled = ref(true);
 const nextDisabled = ref(false);
-const numPages = ref(1);
+const numberOfPages = ref(1);
 const itemsPerPage = ref(5);
 const centerNumber = ref(3);
 const startNumber = ref(1);
-
 const props = defineProps({
   pages: { type: Array },
   // itemsPerPage: { type: Number, default: 5 },
@@ -40,7 +45,7 @@ const props = defineProps({
 });
 
 onMounted(() => {
-  numButtons.value = props.radius * 2 + 1;
+  numberOfButtons.value = props.radius * 2 + 1;
   startNumber.value = 1;
   centerNumber.value = props.radius + 1;
   setButtonNumbers();
@@ -53,13 +58,15 @@ onMounted(() => {
 
 onUpdated(() => {
   console.log("IN onUpdated");
-  setActiveButton(currentPage.value);
+  // setActiveButton(currentPage.value);
 });
 
 const pageButtonClicked = (page, event) => {
   // console.log(page);
+  console.log("===================================================");
   console.log("Page: ", page);
   console.log("Target textContent: ", event.target.textContent);
+  console.log("CenterNumber: ", centerNumber.value);
   currentPage.value = page;
   if (centerNumber.value != page) {
     console.log("Shift numbers");
@@ -70,8 +77,8 @@ const pageButtonClicked = (page, event) => {
     setButtonNumbers();
   }
   // centerNumber.value = page;
-  numPages.value = Math.ceil(props.totalItems / itemsPerPage.value);
-  if (currentPage.value > numPages.value - 1) {
+  numberOfPages.value = Math.ceil(props.totalItems / itemsPerPage.value);
+  if (currentPage.value > numberOfPages.value - 1) {
     nextDisabled.value = true;
   }
 
@@ -79,54 +86,80 @@ const pageButtonClicked = (page, event) => {
 };
 
 const setButtonNumbers = () => {
-  for (var i = 0; i < numButtons.value; i++) {
+  for (var i = 0; i < numberOfButtons.value; i++) {
     buttonNumbers.value[i] = i + startNumber.value;
   }
 };
 
 const shiftButtonNumbers = () => {
   console.log("Shift buttons numbers.");
+
+  // If the current selected page and the current center number
+  // are the same, don't shift.
   if (centerNumber.value === currentPage.value) {
     return;
   }
+
+  // Clear the button numbers array
   buttonNumbers.value = [];
+
+  // If the center number is greater than the current page selected,
+  // then we are descending. Set the button numbers...
   if (centerNumber.value > currentPage.value) {
-    let delta = currentPage.value - centerNumber.value;
+    // Get the delta between the two values
+    let delta = centerNumber.value - currentPage.value;
+
+    // If the current starting number minus the delta is <=1, then
+    // we need the start number to be 1.
+    // Center number is the radius + 1.
+    // Start number is 1.
+    // Set the button number values.
     if (startNumber.value - delta <= 1) {
       centerNumber.value = props.radius + 1;
       startNumber.value = 1;
-      for (var i = 0; i < numButtons.value; i++) {
+      for (var i = 0; i < numberOfButtons.value; i++) {
         buttonNumbers.value[i] = i + startNumber.value;
       }
     }
+    // Otherwise, the starting number minus the delta > 1.
+    // We are not at the beginning of the range.
+    // Subtract delta from the starting and center numbers.
+    // Set the button number values.
+    else {
+      startNumber.value -= delta;
+      centerNumber.value -= delta;
+      for (var j = 0; j < numberOfButtons.value; j++) {
+        buttonNumbers.value[j] = j + startNumber.value;
+      }
+    }
+
+    // Otherwise, we are ascending. Set the button numbers.
+    // Determine the delta and set the values accordingly.
   } else {
     let delta = currentPage.value - centerNumber.value;
-    console.log("Delta: ", delta);
     startNumber.value += delta;
-    console.log("Start Num: ", startNumber.value);
-    for (var j = 0; j < numButtons.value; j++) {
-      buttonNumbers.value[j] = j + startNumber.value;
+    for (var k = 0; k < numberOfButtons.value; k++) {
+      buttonNumbers.value[k] = k + startNumber.value;
     }
     centerNumber.value = currentPage.value;
-    console.log("Center number: ", centerNumber.value);
   }
 };
 
 const setActiveButton = (activeNumber) => {
-  console.log("IN setActiveButton");
-  console.log("setActiveButton: activeNumber = ", activeNumber);
+  // console.log("IN setActiveButton");
+  // console.log("setActiveButton: activeNumber = ", activeNumber);
   // Remove all active items
   var activeItems = document.getElementsByClassName("qam-current");
   for (var i = 0; i < activeItems.length; i++) {
-    console.log("setActiveButton: remove class from item = ", i);
+    // console.log("setActiveButton: remove class from item = ", i);
     activeItems[i].classList.remove("qam-current");
   }
 
   var pageButtons = document.getElementsByClassName("qam-page-button");
   for (var j = 0; j < pageButtons.length; j++) {
-    console.log("setActiveButton: textContent = ", pageButtons[j].textContent);
+    // console.log("setActiveButton: textContent = ", pageButtons[j].textContent);
     if (pageButtons[j].textContent == activeNumber) {
-      console.log("setActiveButton: add class to item = ", j);
+      // console.log("setActiveButton: add class to item = ", j);
       pageButtons[j].classList.add("qam-current");
     }
   }
@@ -134,7 +167,8 @@ const setActiveButton = (activeNumber) => {
   // el.classList.add("qam-current");
 };
 const checkPreviousAndNext = () => {
-  if (currentPage.value === 1) {
+  var center = props.radius + 1;
+  if (currentPage.value <= center) {
     prevDisabled.value = true;
   } else {
     prevDisabled.value = false;
